52fb875d74db13b031e69b50198724218e07a502
Sam-Abood <abdlwahdsa@gmail.com>
2016-08-23 14:08:15 -0600

    Add letters drawing

--- Fiddle 1/main.js
+++ Fiddle 1/main.js
@@ -1,8 +1,8 @@
 var width, height;
 var Mouse = {
-	x: 0.0,
-	y: 0.0,
-	r: 150.0
+	x: width*2,
+	y: 0,
+	r: 90.0
 };
 
 function distSq(v1,v2){
@@ -81,6 +81,7 @@ function Particle(CX,CY,R) {
 
 }
 
+// push particles away from an intersecting circle
 Particle.prototype.constrain = function(m,rad) { // assumes rad is way bigger than this.r
 	var dist = distSq(m,this.pos);
 	if(dist < Math.pow(rad+this.r,2)){
@@ -93,48 +94,86 @@ Particle.prototype.constrain = function(m,rad) { // assumes rad is way bigger th
 	}
 };
 
-function PSys(){
+// function PSys(){
+// 	var parts = [];
+
+// 	for (var i = 1; i <= 10; i++) {
+// 		addRing(i*60, i*25, 1);
+// 	}
+// 	this.count = parts.length;
+
+// 	function addRing(count,radius,pRad) {
+// 		var ang = Math.random()*Math.PI*2, incr = Math.PI*2.0/count;
+// 		for(var i=0; i < count; i++) {
+// 			parts.push(
+// 				new Particle(width/2.0+Math.cos(ang)*radius, height/2.0+Math.sin(ang)*radius, pRad, 0,0)
+// 			);
+// 			ang += incr;
+// 		}
+// 	}
+
+// 	this.draw = function(context){
+// 		for(var i =0; i <parts.length; i++)
+// 			parts[i].draw(context);
+// 	};
+// 	this.update = function(){
+// 		for(var i =0; i <parts.length; i++)
+// 			parts[i].update();
+// 	};
+// };
+
+// creates a particle array from an image with particles positioned at non-white pixels
+function imageToParticles(imgObj, scaleX, scaleY, offX, offY,rad) { 
 	var parts = [];
 
-	for (var i = 1; i <= 10; i++) {
-		addRing(i*60, i*25, 1);
-	}
-	this.count = parts.length;
+	for(var i = 0; i < imgObj.data.length; i += 4) {
+		var px = i/4;
+		var x = px % imgObj.w,
+			y = px / imgObj.w;
+		// we'll accept a pixel if it's non-white (its R, G and B values are less than a whitness bar)  (ignoreing alpha)
+		var bar = 200
+		var nonWhite = (imgObj.data[i] < bar)  || (imgObj.data[i + 1] < bar) || (imgObj.data[i + 2] < bar);
 
-	function addRing(count,radius,pRad) {
-		var ang = Math.random()*Math.PI*2, incr = Math.PI*2.0/count;
-		for(var i=0; i < count; i++) {
-			parts.push(
-				new Particle(width/2.0+Math.cos(ang)*radius, height/2.0+Math.sin(ang)*radius, pRad, 0,0)
-			);
-			ang += incr;
-		}
+		if(nonWhite)
+			if( Math.random() < 0.5)	// too many pixels/particles!! We'll introduce a chance to whether we'll create a particle or not
+				parts.push(new Particle(x*scaleX + offX, y*scaleY + offY, rad));
 	}
 
-	this.draw = function(context){
-		for(var i =0; i <parts.length; i++)
-			parts[i].draw(context);
-	};
-	this.update = function(){
-		for(var i =0; i <parts.length; i++)
-			parts[i].update();
-	};
-};
+	 return parts;
+}
 
 window.onload = function() {
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d");
 	width = canvas.width = window.innerWidth;
 	height = canvas.height = window.innerHeight;
-	var psystem = new PSys();
+	//var psystem = new PSys();
 	var showMouse = false;
+	var btn = document.getElementById('btn');
+	var txt = document.getElementById('txt');
+	var letters = [];
+
+
 	addEventListeners();
+	setupNewWord("test");
 	run();
+
+	function setupNewWord(word) {	
+		word = word.toLowerCase();
+		var perLine = 5, spacing = 150, scl = 2;
+		letters = [];
+		for(var i = 0; i < word.length; i++) {
+			var ascii = word.charCodeAt(i);
+			if(ascii < 97 || ascii > 122) continue;
+			letters.push(imageToParticles(images[ascii - 97],scl,scl, 50+ scl*((i*spacing)%(spacing*perLine)), height/2 - 100, 2));
+		}
+	}
 
 	function addEventListeners(){
 		window.onresize = function(event) {
 
 		};
+
 		document.body.addEventListener("mousemove", function(event) {
 			Mouse.x = event.clientX;
 			Mouse.y = event.clientY;
@@ -147,15 +186,30 @@ window.onload = function() {
 		document.body.addEventListener("wheel", function(WheelEvent) {
 			 Mouse.r += WheelEvent.deltaY < 0 ? 5.0 : -5.0;
 		});
+
 		window.onkeydown = function(e) {
 		
+		};
+
+		btn.onclick = function(){
+			setupNewWord(txt.value)
 		};
 	}
 
 	function run() {		
 		context.clearRect(0, 0, width, height);
-		psystem.update();
-		psystem.draw(context);
+		// psystem.update();
+		// psystem.draw(context);
+
+		var partsCount = 0;
+		for(var j =0; j <letters.length; j++){
+		 	var parts = letters[j];
+		 	partsCount += parts.length;
+			for (var i = 0; i < parts.length; i++) {
+				parts[i].update();
+				parts[i].draw(context);
+		 	}
+		 }
 
 		// draw mouse's circle
 		if(showMouse){
@@ -165,15 +219,16 @@ window.onload = function() {
 			context.fill();
 			context.closePath();
 		}
+
 		context.textAlign = 'left';
-	    context.font = '20px sans-serif';
+	    context.font = '14px sans-serif';
 	    context.fillStyle = 'gray';
-		context.fillText("particles: "+psystem.count, 25, 30);
-		context.fillText("mouse click: show effect circle", 25, 60);
-		context.fillText("mouse wheel: change effect radius", 25, 90);
+		context.fillText("particles: "+partsCount, 15, 25);
+		//context.fillText("particles: "+psystem.count, 25, 30);
+		context.fillText("mouse click: show brush", 15, 50);
+		context.fillText("mouse wheel: change brush radius", 15, 75);
 
 		requestAnimationFrame(run);
 	}	
-
 
 };
---
+++ Fiddle 1/preload.js
@@ -0,0 +1,29 @@
+function imageObject(W,H,raw) {
+	this.w = W;
+	this.h = H;
+	this.data = raw;
+}
+
+function loadImages() { // create imageObject for each image in the Letters folder, and store it in images[]
+    for (var i = 0; i < 26; i++) {
+    	var img = new Image();
+    	img.i = i;	
+
+    	img.addEventListener("load", function() {
+		    var tempCanvas = document.createElement('canvas');
+			tempCanvas.height = this.height;
+			tempCanvas.width = this.width;
+			var tempCtx = tempCanvas.getContext('2d');
+			tempCtx.drawImage(this, 0, 0); // Draw image on temporary canvas
+			var data = tempCtx.getImageData(0, 0, this.width, this.height).data;
+			console.log("Loaded image "+this.i);
+			images[this.i] = new imageObject(this.width, this.height, data.slice());
+			// tempCanvas stuff should garabage-collect?
+		});
+
+		img.src = "Letters/"+i+".png";
+    }
+}
+
+var images = [];
+loadImages();
